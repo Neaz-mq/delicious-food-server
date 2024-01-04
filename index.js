112b6fbbfdb7166d8e819528ce8829aef3c94be6
Neaz-mq <neazmorshed666@gmail.com>
2024-01-05 00:42:45 +0600

    reviews added

--- index.js
+++ index.js
@@ -27,11 +27,17 @@ async function run() {
   
 
     const menuCollection = client.db("foodDb").collection("menu");
+    const reviewCollection = client.db("foodDb").collection("reviews");
     
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
     })
+
+    app.get('/reviews', async(req, res) =>{
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+  })
 
 
 
