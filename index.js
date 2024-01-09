08162e2075a3a435d70473a1f176c096b4840e5e
Neaz-mq <neazmorshed666@gmail.com>
2024-01-09 11:55:15 +0600

    added user post

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zyo6s3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,11 +25,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
   
-
+    const userCollection = client.db("foodDb").collection("users");
     const menuCollection = client.db("foodDb").collection("menu");
     const reviewCollection = client.db("foodDb").collection("reviews");
     const cartCollection = client.db("foodDb").collection("carts");
     
+      // users related api
+
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+        // insert email if user doesnt exists: 
+        // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+        // const query = { email: user.email }
+        // const existingUser = await userCollection.findOne(query);
+        // if (existingUser) {
+        //   return res.send({ message: 'user already exists', insertedId: null })
+        // }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      });
+  
+  
+
+
+
+
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
@@ -55,7 +75,14 @@ async function run() {
     const cartItem = req.body;
     const result = await cartCollection.insertOne(cartItem);
     res.send(result);
-  })
+  });
+
+  app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  });
 
 
 
